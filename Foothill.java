9bce463037c84a105a4a44576b7b580bb7374b14
Ellis Sentoso <ellis95official@gmail.com>
2019-07-05 16:06:30 -0700

    Add files via upload

---
+++ Foothill.java
@@ -0,0 +1,36 @@
+/* Name: Ellis Sentoso
+ * Instructor: Dave Harden
+ * Assignment: Lab1 CS1
+ * Email: ellissentoso@gmail.com
+ * Date: 06/05/2019
+ */
+public class Foothill {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("My full name is Ellis Sentoso");
+		System.out.println("\n Detail #1: Write CS1A in the subject to when emailing our Professor");
+		System.out.println("\n Detail #2: Students can submit up to 48 hours late without gettting penalized" );
+		System.out.println("\n Detail #3: Inappropriate collaboration would receives 0 on assignment for first timer");
+		System.out.println("\n Detail #4: Inappropriate collaboration receives an F for a second attempt");
+		System.out.println("\n Detail #5: No assignments will be accepted after Wednesday, Aug 7");
+
+
+	}
+
+}
+
+/*------- Sample Run -------------------------------------------------------
+Hello World!
+My full name is Ellis Sentoso
+
+ Detail #1: Write CS1A in the subject to when emailing our Professor
+
+ Detail #2: Students can submit up to 48 hours late without gettting penalized
+
+ Detail #3: Inappropriate collaboration would receives 0 on assignment for first timer
+
+ Detail #4: Inappropriate collaboration receives an F for a second attempt
+
+ Detail #5: No assignments will be accepted after Wednesday, Aug 7
+ ------------------------------------------------------------------------*/
